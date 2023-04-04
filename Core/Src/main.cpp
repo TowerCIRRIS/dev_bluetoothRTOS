d070e6d8f8cd15e8ed58f402c27111c69e43ee92
TowerCIRRIS <72414879+TowerCIRRIS@users.noreply.github.com>
2023-04-04 10:36:35 -0400

    ajusté flash latency poru être comme dans le projet Ergowatch, fonctinnel

--- Core/Src/main.cpp
+++ Core/Src/main.cpp
@@ -211,7 +211,7 @@ void SystemClock_Config(void)
   RCC_ClkInitStruct.AHBCLK2Divider = RCC_SYSCLK_DIV2;
   RCC_ClkInitStruct.AHBCLK4Divider = RCC_SYSCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_3) != HAL_OK)
   {
     Error_Handler();
   }
